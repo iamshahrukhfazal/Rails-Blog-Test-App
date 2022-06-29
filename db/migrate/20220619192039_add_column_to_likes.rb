c0f8aa79cf4b71f9a01343da162c9d485006f1a4
imshahrukh <sk902223@gmail.com>
2022-06-29 15:15:51 +0500

    update the true from true in migrations

--- db/migrate/20220619192039_add_column_to_likes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Service to download ftp files from the server
-class AddColumnToLikes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :likes, :likeable_type, :string
-  end
-end
