2b55c73735644c79d8ee2b9efe3e6ed78e912066
imshahrukh <sk902223@gmail.com>
2022-06-29 16:09:48 +0500

    Remove Conformables

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :lockable, :confirmable
+         :recoverable, :rememberable, :validatable, :lockable, 
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
