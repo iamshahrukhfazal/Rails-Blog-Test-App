30f6df114a137b1477b55904b3d09e1581d408dc
imshahrukh <sk902223@gmail.com>
2022-06-29 15:42:37 +0500

    update the true from true in migrationss

--- db/migrate/20220628090630_addlikeableid_andlikeable_type_uniqness.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddlikeableidAndlikeableTypeUniqness < ActiveRecord::Migration[5.2]
-  def change
-    add_index :likes, %i[likeable_id likeable_type], unique: true
-  end
-end
